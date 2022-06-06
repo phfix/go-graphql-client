a4655423ed1b4d39555236763c2da42ad92f67e6
David Bain <97858950+david-bain@users.noreply.github.com>
2022-06-06 14:39:23 +1000

    internal/jsonutil: support directives directly after name
    Also consider "@" when looking for the end of the field name, since
    GraphQL directives can follow a field name immediately without any
    arguments or aliases being involved. For example:
    
    	query {
    		me {
    			firstName
    			lastName @include(if: $expandedInfo)
    		}
    	}
    
    The strings.Index check for "@" is the third separator we look for.
    There's no reason to look separately, since we just want the first
    field name separator.
    
    Add test covering a GraphQL directive that immediately follows the
    field name.
    
    Co-authored-by: Dmitri Shuralyov <dmitri@shuralyov.com>
    GitHub-Pull-Request: https://github.com/shurcooL/graphql/pull/94

--- pkg/jsonutil/graphql_test.go
+++ pkg/jsonutil/graphql_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/hasura/go-graphql-client"
 	"github.com/hasura/go-graphql-client/pkg/jsonutil"
 )
 
@@ -446,6 +447,39 @@ func TestUnmarshalGraphQL_multipleValuesInOrderedMap(t *testing.T) {
 	}
 	if got, want := err.Error(), "invalid token '{' after top-level value"; got != want {
 		t.Errorf("got error: %v, want: %v", got, want)
+	}
+}
+
+func TestUnmarshalGraphQL_directives(t *testing.T) {
+	/*
+		query {
+			me {
+				name @include(if: true)
+				height @skip(if: false)
+			}
+		}
+	*/
+	type query struct {
+		Me struct {
+			Name   graphql.String `graphql:"name @include(if: true)"`
+			Height graphql.Float  `graphql:"height @skip(if: false)"`
+		}
+	}
+	var got query
+	err := jsonutil.UnmarshalGraphQL([]byte(`{
+		"me": {
+			"name": "Luke Skywalker",
+			"height": 1.72
+		}
+	}`), &got)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var want query
+	want.Me.Name = "Luke Skywalker"
+	want.Me.Height = 1.72
+	if !reflect.DeepEqual(got, want) {
+		t.Error("not equal")
 	}
 }
 
